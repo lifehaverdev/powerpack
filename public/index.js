f680d56ea112dade603d2ce9d9f491fa4744354e
jfkwebdev <jfkwebdev@gmail.com>
2023-10-20 20:33:33 -0500

    node so local server can play with metamask foundry evm

---
+++ public/index.js
@@ -0,0 +1,419 @@
+/*
+
+ ________  ___   __    ______   ______   __     __     
+/_______/\/__/\ /__/\ /_____/\ /_____/\ /__/\ /__/\    
+\__.::._\/\::\_\\  \ \\:::_ \ \\::::_\/_\ \::\\:.\ \   
+   \::\ \  \:. `-\  \ \\:\ \ \ \\:\/___/\\_\::_\:_\/   
+   _\::\ \__\:. _    \ \\:\ \ \ \\::___\/_ _\/__\_\_/\ 
+  /__\::\__/\\. \`-\  \ \\:\/.:| |\:\____/\\ \ \ \::\ \
+  \________\/ \__\/ \__\/ \____/_/ \_____\/ \_\/  \__\/
+                                                       
+
+*/
+
+
+//VARIABLES
+    //state
+    var phase;
+    var hear = false;
+    var character = 12;
+    var character1 = 8;
+    var arena = 1;
+    var curInt;
+    var walletPacks = [];
+    //betting
+    var multiMax = 9;
+    var odds = .5;
+    //game
+    var win = 0;
+    var streak = 0;
+    var stocks = 3;
+    var random = [];
+    var score = [];
+    var round;
+
+//working
+
+var game = {
+    p1: {
+        stock: 0
+    },
+    p2: {
+        stock: 0
+    }
+}
+
+function addExp(packId) {
+    if(userExp[packId]){
+        //console.log('pack found');
+        userExp[packId] += 50 * (.5 / odds) ;
+    } else {
+        //console.log('fresh pack');
+        userExp[packId] = 50 * (.5 / odds ) ;
+    }
+    console.log(userExp)
+}
+
+function calculatePrize() {
+    prize = wager * ((1-odds) / .5) * ((1 - odds) / .5);
+}
+
+//sound
+
+var sound = new Howl({
+    src: ['sounds/song.mp3'],
+    html5: true,
+    loop: true
+  });
+
+var menu = new Howl({
+    src: ['sounds/time.mp3'],
+    html5: true,
+    loop: true
+})
+
+function mainMenu(){
+    phase = "main"
+    if(walletPacks.length > 0){
+        frame("","inside","option","option",
+            create("ul","","list","",
+                create("li","","option","",
+                    create("button","","float","charMenu()","Campaign")
+                )
+                +
+                create("li","","option","",
+                    create("button","multi","float","multiFlow()","Create Multiplayer Game")
+                )
+                +
+                create("li","","option","",
+                    create("button","join","float","joinFlow()","Join Multiplayer Game")
+                )
+            )
+        )
+        get("multi").disabled = true;
+        get("join").disabled = true;
+    } else {
+        frame("","inside","option","option",
+            create("p","","","","We don't see any packs in your wallet, you need to register your packs to fight. Please switch to ethereum mainnet and click register")
+            +
+            create("ul","","list","",
+                create("li","","option","",
+                    create("button","","float","register()","Register")
+                )
+                +
+                create("li","","option","",
+                    create("button","","float","charMenu()","Play Offline")    
+                )
+            )
+        )
+    }
+}
+
+// solo
+
+function charMenu(){
+    phase = "char"
+    position = { x: 0, y: 0};
+    frame("","inside","","",  
+        create("div","char-sel","float grid","",
+            `${charList()}`
+        )
+        +
+        create("div","disc","draggable","","")
+        +
+        create("div","picked","card","",
+            `<img src="" alt="pick"/>`
+        )
+        +
+        create("div","spec","","",
+            create("p","exp","stat","","exp: ")
+            +
+            create("p","name","stat","","name: ")
+        )
+        +
+        create("div","param","","",
+            create("div","","dial","",
+                create("p","odds","","","STOCKS: 3")
+                +
+                create("button","risk-less","ctrl","risk(0)","-")
+                +
+                create("button","risk-more","ctrl","risk(1)","+")
+            )
+        )
+    )
+    checkChain("play",0);
+}
+
+function stageMenu() {
+    phase = "stage"
+    position = { x: 0, y: 0};
+    frame("","inside","","",  
+        create("div","stage-sel","float grid","",
+            `${stageList()}`
+        )
+        +
+        create("div","disc","draggable","","")
+        
+    )
+}
+
+function tote() {
+    frame("","","option","",
+        create("div","summary","float sheet","",
+            `${summary()}`
+        )
+        +
+        create("button","fight","","battle()","FIGHT")
+        +
+        create("button","check","","charMenu()","wait")
+    )
+}
+
+function battle() {
+    // if(hear){
+    //     menu.stop()
+    //     sound.play()
+    // }
+    round = 0;
+    writeChain("fight");
+    game.p1.stock = stocks;
+    game.p2.stock = game.p1.stock;
+    frame("","","match","",
+        create("div","action","","",
+            `${action()}`
+        )
+    )
+    get('player1').style.display = 'none';
+    get('player2').style.display = 'none';
+    fight()
+}
+
+function fight() {
+    spawnSlide('player1');
+    spawnSlide('player2');    
+    
+    setTimeout(()=>{
+        get('action').innerHTML = action();
+    },cadence.fight.spawn)
+
+    setTimeout(()=>{
+        countDown()
+    },cadence.fight.countdown)
+
+    setTimeout(()=>{
+        battle1();
+    },cadence.fight.battle)
+}
+
+function action() {
+    return  create("div","game","","",
+                `<img src="./stage/${arena}.png" alt="stage" id="arena" /> `
+                +
+                `<img src="./char/${character}.png" alt="char0" id="player1" />`
+                +
+                `<img src="./char/${character1}.png" alt="char1" id="player2" />`
+                +
+                getStocks()
+            )
+}
+
+function getStocks() {
+    let p1s = `<div class="stocks stock0">`
+    let p2s = `<div class="stocks stock1">`
+    for(let i = 0; i < stocks; i++){
+        if(game.p1.stock > i){
+            p1s += `<img src="./char/${character}.png" alt="stock0" id="0stock${i+1}" class="stock0"/>`;
+        }
+        if(game.p2.stock > i){
+            p2s += `<img src="./char/${character1}.png" alt="stock1" id="1stock1${i+1}" class="stock1"/>`;
+        }
+    }
+    p1s += "</div>"
+    p2s += "</div>"
+    return p1s + p2s;
+}
+
+function result() {
+    // if(hear){
+    //     sound.stop()
+    //     menu.play()
+    // }
+    clearInterval(curInt);
+    if(game.p1.stock > 0){
+        victory();
+    } else if(game.p2.stock > 0){
+        defeat();
+    }
+    resetField();
+    gloat();
+}
+
+function resetField() {
+    game.p1.stock = stocks;
+    game.p2.stock = stocks;
+}
+
+function gloat() {
+    frame("","","","water",
+        create("div","board","","",
+            create("h1","result","","","")
+        )
+    )
+    if(win == 1){
+        get('result').innerHTML = "YOU WIN"
+    } else if(win == -1){
+        get('result').innerHTML = "YOU LOSE"
+    } else {
+        get('result').innerHTML = "something rong"
+    }
+    var butt =  
+        create("button","","","charMenu()","select Pack")
+        +
+        create("button","","","stageMenu()","select Stage")
+        +
+        create("button","","","tote()","again")
+        +
+        create("button","","","mainMenu()","main menu");
+    
+    get('board').innerHTML += butt;
+}
+
+function transaction() {
+
+}
+
+//multi new
+function newGameFlow(){
+    frame(
+        "choose the param of your game"
+    )
+}
+
+function joinFlow(){
+    cast(
+        create("div","","container","",
+            create("div","","container","",
+                gameList()
+            )
+        )
+    )
+}
+
+function charList(){
+    var chars = "";
+    
+    for(i = 1; i < 13; i++){
+        chars += create("div",`${i}`,"char op","",
+            `<img src="./char/${i}.png" alt="char${i}" class="pp"/>`    
+        )
+    }
+    return chars
+}
+
+function stageList() {
+    var stages = "";
+    
+    for(i = 1; i < 14; i++){
+        stages += create("div",`${i}`,"dest op","",
+            `<img src="./stage/${i}.png" alt="stage${i}" class="stage" />`    
+        )
+    }
+    return stages
+}
+
+function gameList(){
+    var games = "";
+    var live = checkChain("",0);
+        for(i=0; i < live.length; i++){
+            games += create("div","${i}","game float","",`Game ${live[i]}`+
+                create("button","","join",`joinGame(${live[i]})`,"Join")
+            );
+        }
+    return games;
+}
+
+loadCard = async(id) => {
+    var img = get(id).innerHTML;
+    //get("picked").innerHTML = img;
+    
+    character = parseInt(get(id).id);
+    // console.log('loadcardchar',character)
+    get("picked").innerHTML = 
+        `<img src="char/${character}.png" id="chosen" alt="pack" />`
+    //stats(id)
+    get('picked').children[0].style.classList = "card"
+    //console.log(get('picked').children[0].style.classList)
+    stats(id);
+    banner();
+}
+
+function banner() {
+    if(get('banner')){
+        get('banner').remove();
+    }
+    document.body.innerHTML += 
+    create("div","banner","ribbon","",
+        create("button","banner-min","tiny",`minimize('banner')`,"-")
+        //+
+        // create("h2","sum","","",`${getBet()} $DMT at ${getRisk()} odds for ${getPrize()} $DMT`)
+        +
+        create("button","next","","next()","READY")
+    );
+    bannerSlide('banner');
+    get('disc').style.opacity = ".33";
+}
+
+function minimize(target) {
+    tar = get(target);
+    tar.style.top = "0px";
+    tar.style.height = "10%";
+    tar.innerHTML = create("button","banner-max","tiny","banner()","+");
+    get('disc').style.opacity = "1";
+    get('banner').style.padding = "0%";
+}
+
+risk = (w) => {
+    if(w > 0){
+        stocks++
+    } else {
+        stocks--
+    }
+    get("odds").innerHTML = "STOCKS: " + `${stocks}`;
+    if(stocks < 2){
+        get('risk-less').disabled = true;
+    } else {
+        get('risk-less').disabled = false;
+    }
+
+    if(stocks > 4){
+    get('risk-more').disabled = true;
+    } else {
+        get('risk-more').disabled = false;
+    }
+    //bannerUpdate()
+}
+
+stats = async (charSelId) => {
+        await checkChain("stats",charSelId);
+}
+
+function summary() {
+    sum = 
+    
+    create("h3","q","","","Are you ready to fight?")
+
+    return sum
+}
+
+//full spread
+boot();
+//start()
+//auth()
+//mainMenu();
+//charMenu();
+//banner();
+//tote();
+//stageMenu();
+//tote()
+//battle();
+//spare();
---
+++ public/web3.js
@@ -0,0 +1,223 @@
+/*
+
+ __ __ __   ______    _______   ______      
+/_//_//_/\ /_____/\ /_______/\ /_____/\     
+\:\\:\\:\ \\::::_\/_\::: _  \ \\:::_:\ \    
+ \:\\:\\:\ \\:\/___/\\::(_)  \/_  /_\:\ \   
+  \:\\:\\:\ \\::___\/_\::  _  \ \ \::_:\ \  
+   \:\\:\\:\ \\:\____/\\::(_)  \ \/___\:\ ' 
+    \_______\/ \_____\/ \_______\/\______/  
+                                            
+*/
+
+const web3 = new Web3(Web3.givenProvider); 
+var accounts = [];
+var team;
+var playerXP;
+//var friend;
+//var totalSup;
+//var connected = true;
+//var chainScanPre = 'https://etherscan.io/tx/'
+
+const masterABI = [{"inputs":[{"internalType":"address","name":"p","type":"address"},{"internalType":"uint256","name":"c","type":"uint256"},{"internalType":"uint256","name":"s","type":"uint256"},{"internalType":"uint256","name":"st","type":"uint256"}],"name":"arm","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"game","type":"uint256"},{"internalType":"bool","name":"p0w","type":"bool"}],"name":"result","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_newChance","type":"address"}],"name":"setChance","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_newEXP","type":"address"}],"name":"setExp","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+const expABI = [{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"addValue","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256[6]","name":"","type":"uint256[6]"}],"name":"register","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+const masterAdd = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
+const expAdd = "0xdc64a140aa3e981100a9beca4e685f962f0cf6c9";
+
+const master = new web3.eth.Contract(masterABI, masterAdd);
+const exp = new web3.eth.Contract(expABI, expAdd);
+
+connectWallet = async() => {
+    console.log('you clicked connect')
+    let connected = false;
+    try {
+        accounts = await web3.eth.requestAccounts().then()
+        connected = true;
+        console.log('we see this');
+    } catch(err) {
+        errorTell(err.message);
+    }   
+    if(connected){
+        //var connectbutt = document.getElementById('connectbutt-text');
+        //connectbutt.innerText = 'Connected';
+        
+        var networkId = await web3.eth.net.getId();
+        //console.log(networkId);
+        if (networkId !== 1) {
+        // Show an error message or take other appropriate action
+        alert('change your network to main ethereum')
+        }
+        checkWallet();
+    }
+}
+
+checkWallet = async() => {
+    team = await exp.registrar(accounts[0]).call()
+    console.log('team: ',team);
+}
+
+arm = async() => {
+    game = await master.arm(accounts[0],character,arena,stocks);
+    console.log('arm');
+}
+
+//function insertCoin() {}
+
+//function getProps() {}
+
+//function checkBack() {}
+
+////JAVASCRIPT FAKE WEB3
+
+function fund(amt) {
+    fakeTransaction(`pls pay ${amt} ETH to play`);
+    //amt = parseFloat(amt);
+    // playerWallet = playerWallet - amt;
+    // purse = purse + amt;
+    // table = table + amt;
+    updateBar();
+}
+
+function defeat() {
+    //table = table - parseFloat(getBet());
+    win = -1;
+    updateBar();
+}
+
+function victory() {
+    //bet = parseFloat(bet);
+    //table = table + bet;
+    streak++;
+    win = 1;
+    addExp(character);
+    updateBar()
+}
+
+fakeConnectWallet = async () => {
+    const butt = get("wallet-ask");
+    butt.innerHTML = 
+        `<img src="loading.gif" alt="loading.."/>`
+    // setTimeout(function (){
+    //     console.log("simulating wallet connection");
+    //     mainMenu();
+    // }, 2000)
+    fakeTransaction('connect');
+    // while(thinking){
+    //     //stay here
+    // }
+    walletPacks = await checkChain("checkPacks");
+    mainMenu();
+}
+
+fakeTransaction = (w) => {
+    alert(w)
+}
+
+writeChain = async(w) => {
+    if(w=="fight"){
+        fakeTransaction('you are now sending an amount of eth on arb to pay for the vrf');
+        score = vrf(stocks*2);
+        // if(hear){
+        //     sound.stop()
+        // }
+    }
+}
+
+checkChain = async(w,i) => {
+    if(w == "games"){
+        setTimeout(function() {
+            console.log('chainchecked');
+            return ["0x3493934","0x9284839","0x838949"];
+            
+        },500)
+    }
+    if(w == "stats"){
+        setTimeout(function() {
+            console.log('chainchekced');
+            console.log(userExp[i])
+            if(userExp[i]){
+                get('exp').innerHTML = "exp: " + `${userExp[i]}`;
+            } else {
+                get('exp').innerHTML = "exp: " + "0";
+            }
+            get('name').innerHTML = "name: " + "";
+            return ["355","3",""];
+        },500)
+    }
+    if(w == "play"){
+        setTimeout(function() {
+            console.log("chainchecked");
+            //get("odds").innerHTML = "odds: " + `${getRisk()}`;
+            //get("wager").innerHTML = "wager: " + `${getBet()} $DMT`;
+        })
+    }
+    if(w == "checkPacks"){
+        //return [171,2,4];
+        return [];
+    }
+}
+
+function vrf(num) {
+    //console.log(score,'inside vrf');
+    let ran = [];
+    for(let i = 0; i < num; i++){
+        // Generate a random number between 0 and 1
+        var random = Math.random();
+        // console.log(random);
+        // If the random number is less than 0.5, consider it heads (true)
+        // Otherwise, consider it tails (false)
+        ran.push(random < parseFloat(.5));
+    }
+    console.log('score',ran)
+    random = ran;
+    return ran;
+}
+
+function simRf(odd) {
+    const random = Math.random();
+    return random < odd;
+}
+
+function getPrizeSim(wage, odd) {
+    return (wage * (.5 / odd));
+}
+
+function printMulti() {
+    for(let i = 0; i < 100; i++){
+        let odd = (i/100);
+        console.log('odd',odd,'multi',(1 * (.5/odd) * (1.5 - odd)))
+    }
+}
+
+function sim(odd) {
+    var play = 100;
+    var tabl = 0;
+    var purs = 1000;
+    var wage;
+    const turn = 1000;
+
+    wage = 1;
+    var count = 10000;
+    while(play > 0 && count > 0 && tabl < purs){
+        if(tabl < wage){
+            play = play - (wage - tabl);
+            tabl = tabl + (wage - tabl);
+            purs = purs + (wage - tabl);
+        }
+        if(tabl > 10){
+            play = play + tabl;
+            purs = purs - tabl;
+            tabl = 0;
+        }
+        var game = simRf(odd);
+        if(game){
+            //win
+            tabl = tabl + getPrizeSim(wage,odd);
+        } else {
+            tabl = tabl - wage;
+        }
+        count --;
+    }
+    console.log(`sim complete for odds ${odd} table:`,tabl,'player',play,'purs',purs)
+
+}
